ce0d8cffc71e2a8f28a6484d8466866b4aa7e2c2
damien911224 <damien911224@gmail.com>
2021-12-24 11:16:23 +0900

    Modified

--- src/testbed_ucf101.py
+++ src/testbed_ucf101.py
@@ -390,7 +390,7 @@ class Networks:
 
         self.is_server = True
         self.batch_size = 16 if self.is_server else 2
-        self.num_gpus = 2 if self.is_server else 1
+        self.num_gpus = 4 if self.is_server else 1
         self.num_workers = self.num_gpus * 20
         self.data_type = "images"
         self.dataset_name = "ucf101"
