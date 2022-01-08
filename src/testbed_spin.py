105d20abba91fe52c7f76eff50467cf3108b7c37
damien911224 <damien911224@gmail.com>
2022-01-09 01:20:35 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -2979,7 +2979,7 @@ class Networks:
             self.dropout_prob = 0.5
 
             self.speed_gamma = 1.0
-            self.rotation_gamma = 0.05
+            self.rotation_gamma = 0.1
 
             if batch_size is None:
                 self.batch_size = \
