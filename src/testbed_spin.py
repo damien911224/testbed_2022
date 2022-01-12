a2995b9fa41addf6468aa8c40181b6439b6f3fe6
damien911224 <damien911224@gmail.com>
2022-01-12 16:44:40 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -957,7 +957,7 @@ class Networks:
                                                          self.dataset_name.upper(),
                                                          "RGB" if self.data_type == "images" else "Flow",
                                                          "Finetuning",
-                                                         "0111"),
+                                                         "0111_no_spin"),
                          "weights.ckpt-{}".format(100))
 
         self.model = self.Model(self, is_training=False, phase="finetuning", data_type=self.data_type)
