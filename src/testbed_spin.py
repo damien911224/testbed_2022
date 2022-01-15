e50ce3b5667a85ec1080b98c4d0a73c012749954
damien911224 <damien911224@gmail.com>
2022-01-16 00:21:35 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -592,7 +592,7 @@ class Networks:
                                                          "UCF101",
                                                          "RGB" if self.data_type == "images" else "Flow",
                                                          "Pretraining",
-                                                         "0114"),
+                                                         "0114_with_speed"),
                          "weights.ckpt-{}".format(60))
 
         self.save_ckpt_file_folder = \
