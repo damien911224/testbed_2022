85f0159d79b9de9c066b694d2ab29db7d0595806
damien911224 <damien911224@gmail.com>
2022-01-18 01:50:39 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -18,7 +18,7 @@ import matplotlib.cm as cm
 class Networks:
 
     def __init__(self):
-        self.input_size = (112, 112, 3)
+        self.input_size = (224, 224, 3)
 
     def pretrain(self, postfix):
         print("=" * 90)
@@ -592,7 +592,7 @@ class Networks:
                                                          "UCF101",
                                                          "RGB" if self.data_type == "images" else "Flow",
                                                          "Pretraining",
-                                                         "0114"),
+                                                         "0117_degree_90"),
                          "weights.ckpt-{}".format(60))
 
         self.save_ckpt_file_folder = \
@@ -4011,4 +4011,4 @@ if __name__ == "__main__":
 
     networks = Networks()
 
-    networks.pretrain(postfix=args.postfix)
+    networks.finetune(postfix=args.postfix)
