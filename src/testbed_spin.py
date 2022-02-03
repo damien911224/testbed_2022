0445a2100f16fa9e0a36ca14e25eda44370908d6
damien911224 <damien911224@gmail.com>
2022-02-03 22:07:05 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -28,7 +28,7 @@ class Networks:
         print("=" * 90)
 
         self.is_server = True
-        self.batch_size = 64 if self.is_server else 2
+        self.batch_size = 16 if self.is_server else 2
         self.num_gpus = 4 if self.is_server else 1
         self.num_workers = self.num_gpus * 24
         self.data_type = "images"
