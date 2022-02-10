0b553697c07e26a2bc77229f9a4128b09de5fda3
damien911224 <damien911224@gmail.com>
2022-02-10 22:06:51 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -20,7 +20,7 @@ import matplotlib.cm as cm
 class Networks:
 
     def __init__(self):
-        self.input_size = (112, 112, 3)
+        self.input_size = (224, 224, 3)
 
     def pretrain(self, postfix):
         print("=" * 90)
@@ -987,7 +987,7 @@ class Networks:
         self.dataset_split = "split01"
         self.flow_type = "tvl1"
         self.optimizer_type = "SGD"
-        self.temporal_width = 64
+        self.temporal_width = 32
         self.dtype = tf.float32
         self.dformat = "NDHWC"
 
@@ -2920,7 +2920,7 @@ class Networks:
                 class_index = int(splits[2])
 
                 target_frames = list()
-                start_indices = np.linspace(0, frame_length - self.dataset.networks.temporal_width, 10, dtype=np.int64)
+                start_indices = np.linspace(0, frame_length - self.dataset.networks.temporal_width, 8, dtype=np.int64)
                 start_indices = np.unique(start_indices)
                 for start_index in start_indices:
                     this_frames = list()
