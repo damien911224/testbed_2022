7bf1bf6de32156bef7312f43c89726660d234e08
damien911224 <damien911224@gmail.com>
2022-03-02 23:27:58 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -32,7 +32,7 @@ class Networks:
         self.num_gpus = 2 if self.is_server else 1
         self.num_workers = self.num_gpus * 24
         self.data_type = "images"
-        self.dataset_name = "ucf101"
+        self.dataset_name = "kinetics"
         self.dataset_split = "split01"
         self.flow_type = "tvl1"
         self.optimizer_type = "SGD"
@@ -4140,4 +4140,4 @@ if __name__ == "__main__":
 
     networks = Networks()
 
-    networks.test(postfix=args.postfix)
+    networks.pretrain(postfix=args.postfix)
