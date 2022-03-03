8e58d51cd03dad9c9edf5653f2dc428af97769e5
damien911224 <damien911224@gmail.com>
2022-03-03 15:12:45 +0900

    Modified

--- src/testbed_spin_03.py
+++ src/testbed_spin_03.py
@@ -37,8 +37,8 @@ class Networks:
         self.flow_type = "tvl1"
         self.optimizer_type = "SGD"
         if self.dataset_name == "ucf101":
-            self.epochs = 60
-            # self.epochs = 180
+            # self.epochs = 60
+            self.epochs = 180
         elif self.dataset_name == "kinetics":
             self.epochs = 120
         self.temporal_width = 16
