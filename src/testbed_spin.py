1771f14e69856ed2c7bfc8ba4e30a63e2cd21c28
damien911224 <damien911224@gmail.com>
2022-03-20 17:07:04 +0900

    Modified

--- src/testbed_spin.py
+++ src/testbed_spin.py
@@ -1013,7 +1013,7 @@ class Networks:
                                                          self.dataset_name.upper(),
                                                          "RGB" if self.data_type == "images" else "Flow",
                                                          "Finetuning",
-                                                         "0228_random_twice_speed_no_first_no_cum"),
+                                                         "0319"),
                          "weights.ckpt-{}".format(100))
 
         self.model = self.Model(self, is_training=False, phase="finetuning", data_type=self.data_type)
@@ -4156,4 +4156,4 @@ if __name__ == "__main__":
 
     networks = Networks()
 
-    networks.finetune(postfix=args.postfix)
+    networks.test(postfix=args.postfix)
